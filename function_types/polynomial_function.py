5af62af8c8f11dbf87538c4b1f7bd5668410fe3c
greekfreak42 <creativelk68@protonmail.com>
2022-11-04 17:38:44 +0100

    Added folling methods to PolynomialFunction-class: __add__, __sub__, negated, __mul__, and _summarize

--- function_types/polynomial_function.py
+++ function_types/polynomial_function.py
@@ -12,14 +12,79 @@ class PolynomialFunction:
         assert len(self.a) == len(self.m)
         n = len(self.a)
 
-        new_a = np.zeros(n)
-        new_m = np.zeros(n)
+        new_a = np.empty(n)
+        new_m = np.empty(n)
 
         for i in range(n):
-            new_a[i] += self.a[i] * self.m[i]
-            new_m[i] += self.m[i] - 1
+            new_a[i] = self.a[i] * self.m[i]
+            new_m[i] = self.m[i] - 1
         
         return PolynomialFunction(new_a, new_m)
+    
+
+    def _summarize(self) -> None:
+        elements = {}
+
+        assert len(self.a) == len(self.m)
+
+        for i in range(len(self.a)):
+            if self.m[i] not in elements.keys(): elements.update({self.m[i] : self.a[i]})
+            else: elements[self.m[i]] += self.a[i]
+        
+        new_size = len(elements)
+        new_a = np.empty(new_size)
+        new_m = np.empty(new_size)
+
+        for i in range(len(new_m)):
+            max_exp = max(elements.keys())
+            new_m[i] = max_exp
+            new_a[i] = elements[max_exp]
+            del elements[max_exp]
+        
+        self.a = new_a
+        self.m = new_m
+    
+
+    def __add__(self, other):
+        result = PolynomialFunction(self.a.copy(), self.m.copy())
+
+        for i, exp in enumerate(other.m):
+            if exp in result.m:
+                idx = np.where(result.m == exp)[0][0]
+                result.a[idx] += other.a[i]
+            else:
+                np.append(result.a, other.a[i])
+                np.append(result.m, other.m[i])
+                result._summarize()
+        
+        return result
+    
+
+    def negated(self): return PolynomialFunction(self.a * -1, self.m.copy())
+
+
+    def __sub__(self, other): return self + other.negated()
+
+
+    def __mul__(self, other):
+        if type(other) is not PolynomialFunction: return PolynomialFunction(self.a * other, self.m.copy())
+
+        assert len(self.a) == len(self.m)
+        assert len(other.a) == len(other.m)
+
+        new_size = len(self.a) * len(other.a)
+
+        new_a = np.zeros(new_size)
+        new_m = np.zeros(new_size)
+
+        for i in range(len(other.a)):
+            for j in range(len(self.a)):
+                new_a[j + i*len(self.a)] = other.a[i] * self.a[j]
+                new_m[j + i*len(self.a)] = other.m[i] + self.m[j]
+        
+        result = PolynomialFunction(new_a, new_m)
+        result._summarize()
+        return result
     
 
     def __str__(self) -> str:
@@ -34,7 +99,12 @@ class PolynomialFunction:
 
             if i > 0: s += ' '
 
-            s += f'{abs(self.a[i])}*x^{str(self.m[i])}'
+            if self.m[i] == 0:
+                s += str(abs(self.a[i]))
+            elif self.m[i] == 1:
+                s += f'{abs(self.a[i])}*x'
+            else:
+                s += f'{abs(self.a[i])}*x^{str(self.m[i])}'
 
             if i < n-1: s += ' '
     
--- test.py
+++ test.py
@@ -5,11 +5,15 @@ import numpy as np
 from function_types.polynomial_function import PolynomialFunction
 
 def main() -> None:
-    f = PolynomialFunction(np.array([-1, 3]), np.array([3, 2]))
-    g = f.get_differentiated()
+    f = PolynomialFunction(np.array([3, 4, -3, 2, 7, -23]), np.array([6, 5, 3, 2, 1, 0]))
+    g = PolynomialFunction(np.array([7, 8]), np.array([3, 2]))
 
-    print(f)
-    print(g)
+    print(f'f(x) = {f}')
+    print(f'g(x) = {g}')
+
+    h = f * g
+
+    print(f'h(x) = f(x) * g(x) = {h}')
 
 
 if __name__ == '__main__': main()
